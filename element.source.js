5febe9871822cb90ee4d52508c5811ebc7128d89
Danny Engelman <danny@engelman.nl>
2025-06-06 09:33:03 +0200

    smaller <file-size> sources

--- element.source.js
+++ element.source.js
@@ -1,21 +1,16 @@
-customElements.define(
-  "file-size",
-  class extends HTMLElement {
+customElements.define("file-size", class extends HTMLElement {
     connectedCallback(
       // BYTE SAVINGS: DECLARE variables/functions as parameters to prevent 4 bytes let statement
       // source file to get content-length of:
-      source = this.getAttribute("src") ||
-        "https://file-size.github.io/element.js",
-      // global state to load gzthermal IMG once
-      img = false,
+      source = this.getAttribute("src") || "https://file-size.github.io/element.js",
+      img, // global state to load gzthermal IMG once
       // display bigger than maxbytes in red color
       maxbytes = this.getAttribute("max") || 49152, // 48 kByte, because my first TRS-80 had 48 kB RAM
-      // load GZThermal IMG function:
-      gzthermal = () => {
+      gzthermal = () => { // load GZThermal IMG function:
         if (!img) {
-          this.querySelector("i").innerHTML = "Loading GZIP Thermal analysis";
+          this.querySelector("i").innerHTML = "loading gzthermal";
           img = this.querySelector("img");
-          img.onerror = (e) => new Error("API rate limit enforced");
+          img.onerror = (e) => new Error("rate limit");
           img.onload = (e) => {
             this.querySelector("i").innerHTML = "";
             img.style.width = (this.getAttribute("width") || "700") + "px";
@@ -30,28 +25,21 @@ customElements.define(
       }
     ) {
       this.setAttribute("title", source);
-      fetch(source, {
-        mode: "cors",
-      })
+      fetch(source, { mode: "cors" })
         .then((response) => {
           this.length = Number(response.headers.get("content-length"));
-
           this.innerHTML =
             `<details style="cursor:pointer;font:16px arial">` +
             `<summary>` +
-            `<b style="color:dark${
-              this.length < maxbytes ? "green" : "red"
-            };display:inline-block;width:4em;text-align:right">${
+          `<b style="color:dark${this.length < maxbytes ? "green" : "red"};display:inline-block;width:4em;text-align:right">${
               this.length
             }</b> &nbsp;Bytes ${source} <i/>` +
             `</summary><img/>` +
             `</details>`;
-
           // toggle display GZThermal IMG
           this.querySelector("details").ontoggle = (e) => {
             this.querySelector("details").open && gzthermal();
           };
-
           if (this.hasAttribute("gz")) gzthermal(); // load GZThermal IMG by default
         })
         .catch((e) => {
